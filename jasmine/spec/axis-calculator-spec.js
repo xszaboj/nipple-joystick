4030d1fd37c0a7259e0e3b2648ba19023f5830e9
Jack <xszaboj@gmail.com>
2016-11-21 09:14:23 +0000

    reorder test remove duplicate tests

--- jasmine/spec/axis-calculator-spec.js
+++ jasmine/spec/axis-calculator-spec.js
@@ -93,7 +93,7 @@ describe("Calculate direction strength.", function(){
       };
     });
 
-    describe("right up", function(){
+    describe("right", function(){
         it("angle:0deg, distance:50 is x:1", function() {
             data.angle.degree = 0;
             data.distance = 50;
@@ -116,6 +116,48 @@ describe("Calculate direction strength.", function(){
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("right");
             expect(myData.x).toBe(0.5);
+        });
+
+        it("angle:360deg, distance:50 is x:1", function() {
+            data.angle.degree = 360;
+            data.distance = 50;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("right");
+            expect(myData.x).toBe(1);
+        });
+
+        it("angle:360deg, distance:0 is x:0", function() {
+            data.angle.degree = 360;
+            data.distance = 0;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("right");
+            expect(myData.x).toBe(0);
+        });
+
+        it("angle:360deg, distance:25 is x:0.5", function() {
+            data.angle.degree = 360;
+            data.distance = 25;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("right");
+            expect(myData.x).toBe(0.5);
+        });
+    });
+
+    describe("right up", function(){
+        it("angle:22.5deg, distance:50 is x:0.25", function() {
+            data.angle.degree = 22.5;
+            data.distance = 50;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("right");
+            expect(myData.x).toBe(0.75);
+        });
+
+        it("angle:22.5deg, distance:25 is x:0.125", function() {
+            data.angle.degree = 22.5;
+            data.distance = 25;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("right");
+            expect(myData.x).toBe(0.375);
         });
 
         it("angle:45deg, distance:50 is x:0.5", function() {
@@ -158,22 +200,6 @@ describe("Calculate direction strength.", function(){
             expect(myData.x).toBe(0.125);
         });
 
-        it("angle:22.5deg, distance:50 is x:0.25", function() {
-            data.angle.degree = 22.5;
-            data.distance = 50;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(0.75);
-        });
-
-        it("angle:22.5deg, distance:25 is x:0.125", function() {
-            data.angle.degree = 22.5;
-            data.distance = 25;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(0.375);
-        });
-
         it("angle:89.9deg, distance:50 is x:0", function() {
             data.angle.degree = 89.9;
             data.distance = 50;
@@ -184,28 +210,20 @@ describe("Calculate direction strength.", function(){
     });
 
     describe("right down", function(){
-        it("angle:360deg, distance:50 is x:1", function() {
-            data.angle.degree = 360;
+        it("angle:337.5deg, distance:50 is x:0.25", function() {
+            data.angle.degree = 337.5;
             data.distance = 50;
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(1);
+            expect(myData.x).toBe(0.75);
         });
 
-        it("angle:360deg, distance:0 is x:0", function() {
-            data.angle.degree = 360;
-            data.distance = 0;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(0);
-        });
-
-        it("angle:360deg, distance:25 is x:0.5", function() {
-            data.angle.degree = 360;
+        it("angle:337.5deg, distance:25 is x:0.125", function() {
+            data.angle.degree = 337.5;
             data.distance = 25;
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(0.5);
+            expect(myData.x).toBe(0.375);
         });
 
         it("angle:315deg, distance:50 is x:0.5", function() {
@@ -230,22 +248,6 @@ describe("Calculate direction strength.", function(){
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("right");
             expect(myData.x).toBe(0.1);
-        });
-
-        it("angle:337.5deg, distance:50 is x:0.25", function() {
-            data.angle.degree = 337.5;
-            data.distance = 50;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(0.75);
-        });
-
-        it("angle:337.5deg, distance:25 is x:0.125", function() {
-            data.angle.degree = 337.5;
-            data.distance = 25;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("right");
-            expect(myData.x).toBe(0.375);
         });
 
         it("angle:292.5deg, distance:50 is x:0.75", function() {
@@ -273,7 +275,7 @@ describe("Calculate direction strength.", function(){
         });
     });
 
-    describe("left up", function(){
+    describe("left", function(){
         it("angle:180deg, distance:50 is x:-1", function() {
             data.angle.degree = 180;
             data.distance = 50;
@@ -296,6 +298,32 @@ describe("Calculate direction strength.", function(){
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("left");
             expect(myData.x).toBe(-0.5);
+        });
+    });
+
+    describe("left up", function(){
+         it("angle:90.1deg, distance:50 is x:~0", function() {
+            data.angle.degree = 90.1;
+            data.distance = 50;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("left");
+            expect(myData.x).toBe(-0.0011111111111110628);
+        });
+
+        it("angle:112.5deg, distance:50 is x:-0.25", function() {
+            data.angle.degree = 112.5;
+            data.distance = 50;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("left");
+            expect(myData.x).toBe(-0.25);
+        });
+
+        it("angle:112.5deg, distance:25 is x:-0.125", function() {
+            data.angle.degree = 112.5;
+            data.distance = 25;
+            var myData = calculateHorizontalData(data);
+            expect(myData.xDirection).toBe("left");
+            expect(myData.x).toBe(-0.125);
         });
 
         it("angle:135deg, distance:50 is x:-0.5", function() {
@@ -322,22 +350,6 @@ describe("Calculate direction strength.", function(){
             expect(myData.x).toBe(-0.1);
         });
 
-        it("angle:112.5deg, distance:50 is x:-0.25", function() {
-            data.angle.degree = 112.5;
-            data.distance = 50;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.25);
-        });
-
-        it("angle:112.5deg, distance:25 is x:-0.125", function() {
-            data.angle.degree = 112.5;
-            data.distance = 25;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.125);
-        });
-
         it("angle:157.5deg, distance:50 is x:-0.75", function() {
             data.angle.degree = 157.5;
             data.distance = 50;
@@ -354,15 +366,7 @@ describe("Calculate direction strength.", function(){
             expect(myData.x).toBe(-0.375);
         });
 
-        it("angle:90.1deg, distance:50 is x:0", function() {
-            data.angle.degree = 90.1;
-            data.distance = 50;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.0011111111111110628);
-        });
-
-        it("angle:180deg, distance:50 is x:1", function() {
+        it("angle:179.9deg, distance:50 is x:~-1", function() {
             data.angle.degree = 179.9;
             data.distance = 50;
             var myData = calculateHorizontalData(data);
@@ -372,28 +376,28 @@ describe("Calculate direction strength.", function(){
     });
 
     describe("left down", function(){
-        it("angle:180deg, distance:50 is x:-1", function() {
-            data.angle.degree = 180;
+        it("angle:180.1deg, distance:50 is x:~-1", function() {
+            data.angle.degree = 180.1;
             data.distance = 50;
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-1);
+            expect(myData.x).toBe(-0.9988888888888889);
         });
 
-        it("angle:180deg, distance:0 is x:0", function() {
-            data.angle.degree = 180;
-            data.distance = 0;
+        it("angle:202.5deg, distance:50 is x:-0.75", function() {
+            data.angle.degree = 202.5;
+            data.distance = 50;
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(0);
+            expect(myData.x).toBe(-0.75);
         });
 
-        it("angle:180deg, distance:25 is x:-0.5", function() {
-            data.angle.degree = 180;
+        it("angle:202.5deg, distance:25 is x:-0.375", function() {
+            data.angle.degree = 202.5;
             data.distance = 25;
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.5);
+            expect(myData.x).toBe(-0.375);
         });
 
         it("angle:225deg, distance:50 is x:-0.5", function() {
@@ -418,23 +422,7 @@ describe("Calculate direction strength.", function(){
             var myData = calculateHorizontalData(data);
             expect(myData.xDirection).toBe("left");
             expect(myData.x).toBe(-0.1);
-        });
-
-        it("angle:202.5deg, distance:50 is x:-0.75", function() {
-            data.angle.degree = 202.5;
-            data.distance = 50;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.75);
-        });
-
-        it("angle:202.5deg, distance:25 is x:-0.375", function() {
-            data.angle.degree = 202.5;
-            data.distance = 25;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.375);
-        });
+        });        
 
         it("angle:247.5deg, distance:50 is x:-0.25", function() {
             data.angle.degree = 247.5;
@@ -452,15 +440,7 @@ describe("Calculate direction strength.", function(){
             expect(myData.x).toBe(-0.125);
         });
 
-        it("angle:180.1deg, distance:50 is x:1", function() {
-            data.angle.degree = 180.1;
-            data.distance = 50;
-            var myData = calculateHorizontalData(data);
-            expect(myData.xDirection).toBe("left");
-            expect(myData.x).toBe(-0.9988888888888889);
-        });
-
-        it("angle:269.9deg, distance:50 is x:0", function() {
+        it("angle:269.9deg, distance:50 is x:~0", function() {
             data.angle.degree = 269.9;
             data.distance = 50;
             var myData = calculateHorizontalData(data);
@@ -481,44 +461,12 @@ describe("Calculate direction strength.", function(){
       };
     });
     describe("up", function(){
-          it("angle:90deg, distance:50 is y:1", function() {
-            data.angle.degree = 90;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("up");
-            expect(myData.y).toBe(1);
-        });
-
-        it("angle:0.1deg, distance:50 is y:0", function() {
+        it("angle:0.1deg, distance:50 is y:~0", function() {
             data.angle.degree = 0.1;
             data.distance = 50;
             var myData = calculateVerticalData(data);
             expect(myData.yDirection).toBe("up");
             expect(myData.y).toBe(0.0011111111111110628);
-        });
-
-        it("angle:180deg, distance:50 is y:0", function() {
-            data.angle.degree = 179.9;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("up");
-            expect(myData.y).toBe(0.0011111111111110628);
-        });
-
-        it("angle:45deg, distance:50 is y:0.5", function() {
-            data.angle.degree = 45;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("up");
-            expect(myData.y).toBe(0.5);
-        });
-
-        it("angle:135deg, distance:50 is y:0.5", function() {
-            data.angle.degree = 135;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("up");
-            expect(myData.y).toBe(0.5);
         });
 
         it("angle:22.5deg, distance:50 is y:0.25", function() {
@@ -529,6 +477,30 @@ describe("Calculate direction strength.", function(){
             expect(myData.y).toBe(0.25);
         });
 
+        it("angle:45deg, distance:50 is y:0.5", function() {
+            data.angle.degree = 45;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("up");
+            expect(myData.y).toBe(0.5);
+        });
+
+        it("angle:90deg, distance:50 is y:1", function() {
+            data.angle.degree = 90;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("up");
+            expect(myData.y).toBe(1);
+        });
+
+        it("angle:135deg, distance:50 is y:0.5", function() {
+            data.angle.degree = 135;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("up");
+            expect(myData.y).toBe(0.5);
+        });
+
         it("angle:157.5deg, distance:50 is y:0.25", function() {
             data.angle.degree = 157.5;
             data.distance = 50;
@@ -536,18 +508,18 @@ describe("Calculate direction strength.", function(){
             expect(myData.yDirection).toBe("up");
             expect(myData.y).toBe(0.25);
         });
+
+        it("angle:179.9deg, distance:50 is y:~0", function() {
+            data.angle.degree = 179.9;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("up");
+            expect(myData.y).toBe(0.0011111111111110628);
+        });   
     });
 
     describe("down", function(){
-          it("angle:279deg, distance:50 is y:-1", function() {
-            data.angle.degree = 270;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("down");
-            expect(myData.y).toBe(-1);
-        });
-
-        it("angle:359.9deg, distance:50 is y:0", function() {
+        it("angle:359.9deg, distance:50 is y:~0", function() {
             data.angle.degree = 359.9;
             data.distance = 50;
             var myData = calculateVerticalData(data);
@@ -555,12 +527,28 @@ describe("Calculate direction strength.", function(){
             expect(myData.y).toBe(-0.0011111111111113958);
         });
 
-        it("angle:180.1deg, distance:50 is y:0", function() {
-            data.angle.degree = 180.1;
+        it("angle:337.5deg, distance:50 is y:-0.25", function() {
+            data.angle.degree = 337.5;
             data.distance = 50;
             var myData = calculateVerticalData(data);
             expect(myData.yDirection).toBe("down");
-            expect(myData.y).toBe(-0.0011111111111110628);
+            expect(myData.y).toBe(-0.25);
+        });
+
+        it("angle:315deg, distance:50 is y:-0.5", function() {
+            data.angle.degree = 315;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("down");
+            expect(myData.y).toBe(-0.5);
+        });
+
+        it("angle:270deg, distance:50 is y:-1", function() {
+            data.angle.degree = 270;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("down");
+            expect(myData.y).toBe(-1);
         });
 
         it("angle:225deg, distance:50 is y:-0.5", function() {
@@ -571,29 +559,21 @@ describe("Calculate direction strength.", function(){
             expect(myData.y).toBe(-0.5);
         });
 
-        it("angle:315deg, distance:50 is y:0.5", function() {
-            data.angle.degree = 315;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("down");
-            expect(myData.y).toBe(-0.5);
-        });
-
-        it("angle:337.5deg, distance:50 is y:0.25", function() {
-            data.angle.degree = 337.5;
-            data.distance = 50;
-            var myData = calculateVerticalData(data);
-            expect(myData.yDirection).toBe("down");
-            expect(myData.y).toBe(-0.25);
-        });
-
-        it("angle:202.5deg, distance:50 is y:0.25", function() {
+        it("angle:202.5deg, distance:50 is y:-0.25", function() {
             data.angle.degree = 202.5;
             data.distance = 50;
             var myData = calculateVerticalData(data);
             expect(myData.yDirection).toBe("down");
             expect(myData.y).toBe(-0.25);
         });
+
+        it("angle:180.1deg, distance:50 is y:~0", function() {
+            data.angle.degree = 180.1;
+            data.distance = 50;
+            var myData = calculateVerticalData(data);
+            expect(myData.yDirection).toBe("down");
+            expect(myData.y).toBe(-0.0011111111111110628);
+        });        
     });
   });
 });
